7a7522561f5654b3078ebf40d5556497f6ce814e
Jacob Errington <git@mail.jerrington.me>
2015-09-12 23:36:49 -0400

    implement the grammar for pattern matches

--- parser/Render.hs
+++ parser/Render.hs
@@ -7,19 +7,27 @@ asep = "\x261D"
 bind = "\x2b01"
 finger = "\x1f4e6"
 
-func params body = "\x1F3F4" ++ intercalate sep params ++ bind ++ body
+lparen = "\x1F31C"
+rparen = "\x1F31B"
+
+func name pats body = "\x1F3F4" ++ name ++ sep ++ intercalate sep pats' ++ bind ++ body
+    where pats' = map (\p -> lparen ++ p ++ rparen) pats
 
 array elems = lbracket ++ intercalate asep elems ++ rbracket
     where lbracket = "\x1F449"
           rbracket = "\x1F448"
 
+str s = quote ++ s ++ quote
+    where quote = "\x270C"
+
 obj :: String -> Maybe String -> String
-obj tag m = finger ++ tag ++ case m of
+obj tag m = finger ++ str tag ++ case m of
     Just val -> val
     Nothing -> ""
 
 int n = concat (zipWith f (show n) (repeat '\x20E3')) ++ "!"
     where f n box = n:box:[]
 
-main = putStrLn $ concat [ func [ "hi" ] $ obj "asyo" $ Just $ int 5
+main = putStrLn $ concat [ func "hi" [obj "asyo" $ Just (int 5)] "yo"
+                         , func "hi" [array [int 3, int 4]] "asdf"
                          ]
