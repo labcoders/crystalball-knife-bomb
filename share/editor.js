b168058176a725c6696c4c7e6ec093e9151e4860
Guillaume <gumgl@users.noreply.github.com>
2015-09-19 22:15:06 -0400

    Emoji tree and selector

--- share/editor.js
+++ share/editor.js
@@ -1,26 +1,99 @@
 
 var Program; // Parse.com class model
 var emojiDB; // Emoji mapping to images
+var emojiByCategory;
 var ckEditor;
-
+var jqxTheme = 'metrodark';
 
 $(document).ready(function() {
 	if (location.hash != "")
 		loadProgram();
 	window.addEventListener("hashchange", loadProgram);
 
-	ckEditor = $("#source").ckeditor({
-		uiColor: '#9AB8F3',
-		customConfig: ''
+	$('#mainSplitter').jqxSplitter(
+		{theme: jqxTheme,
+		width: '100%',
+		height: '100%',
+		orientation: 'vertical',
+		panels: [{ size: 300, collapsible: true},{collapsible: false}]
 	});
+    $('#leftSplitter').jqxSplitter(
+    	{theme: jqxTheme,
+    	width: '100%',
+    	height: '100%',
+    	orientation: 'horizontal',
+    	panels: [{ size: '30%'}]
+    });
+    $('#rightSplitter').jqxSplitter(
+    	{theme: jqxTheme,
+    	width: '100%',
+    	height: '100%',
+    	orientation: 'horizontal',
+    	panels: [{ size: '60%'}]
+    });
 
 	Parse.initialize("rRc5MWWI4vyZEeshbvZFA1Nz4jKP6UCrgILPXQG3", "yPXq3zWBjYJpoOFsZWDisXYXdKxv6fblQdIMw5Nn");
 	Program = Parse.Object.extend("Program");
 
 	$.getJSON("emoji.json", function(data) {
 		emojiDB = data;
+		emojiByCategory = _.groupBy(emojiDB, 'category');
+
+		var emojiTree = _.mapObject(emojiByCategory, function(emojis,category){
+			return {
+				label: category,
+				items: _.map(emojis, function(emoji) {
+					return {
+						icon: emoji2URL(emoji),
+						label: emoji.name
+					}
+				})
+			}
+		});
+		$('#constructs').jqxTree({ source: emojiTree, theme: jqxTheme, width: '100%', height: '100%'});
+
+		$('#all-emoji').jqxTabs({
+			width: '100%',
+			height: '100%',
+			theme: jqxTheme,
+			animationType: 'fade',
+			selectionTracker: true
+		});
+
+		for (var categoryName in emojiByCategory) {
+			$('#all-emoji').jqxTabs('addLast', categoryName, 'Loading...');
+		}
+
+		$('#all-emoji').jqxTabs('initTabContent', function(tab){
+			//alert("yo "+tab);
+			var content = "";
+			console.log($('#all-emoji').jqxTabs('getTitleAt', tab));
+			emojiByCategory[$('#all-emoji').jqxTabs('getTitleAt', tab)].forEach(function(emoji) {
+				content += emoji2img(emoji);
+			});
+			$('#all-emoji').jqxTabs('setContentAt', tab, content);
+		});
 	});
 });
+
+function emoji2URL(emoji) {
+	return 'http://cdn.jsdelivr.net/emojione/assets/png/'+emoji.unicode+'.png?v=1.2.4';
+}
+
+function emoji2img(emoji) {
+	//console.log(emoji.unicode, Number('0x'+emoji.unicode))
+	return '<img src="'+emoji2URL(emoji)+'" class="emojione" />';//alt="'+String.fromCodePoint(Number('0x'+emoji.unicode))+'" />';
+	//return emojione.toImage(emoji.shortname)
+}
+
+function searchEmoji(str) {
+	var content = "";
+	console.log($('#all-emoji').jqxTabs('getTitleAt', tab));
+	emojiByCategory[$('#all-emoji').jqxTabs('getTitleAt', tab)].forEach(function(emoji) {
+		content += emoji2img(emoji);
+	});
+	$('#all-emoji').jqxTabs('setContentAt', tab, content);
+}
 
 var charToIMG = function(c) {
 	
